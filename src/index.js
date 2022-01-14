3a16f1fc984bbd06f379e8ce5de798ba89155126
okhun <okhunjongaybullaev@gmail.com>
2022-01-14 14:56:15 +0500

    creating react element

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+// Creating react element using createElement
+const title = React.createElement("h1", {}, "My First React Code");
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  title,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
