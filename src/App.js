552115bf73c618625a4e8ffd8cd5beb6f6ce66bd
okhun <okhunjongaybullaev@gmail.com>
2022-01-14 16:37:56 +0500

    Function component and counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div>
+        <button
+          onClick={() => {
+            let temp = count + 1;
+            setCount(temp);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Increment
+        </button>
+        <p>{count}</p>
+        <button
+          onClick={() => {
+            let temp = count - 1;
+            setCount(temp);
+          }}
+        >
+          Decrement
+        </button>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,10 +7,7 @@ import PureComponent from "./PureComponent";
 import App from "./App";
 // Creating react element using createElement
 
-ReactDOM.render(
-  <PureComponent></PureComponent>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App></App>, document.getElementById("root"));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
